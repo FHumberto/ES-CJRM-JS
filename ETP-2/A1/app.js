c03138aa2f907d8e5026e16caddd8408e770211a
Humberto Guedes <franciscohguedes@gmail.com>
2022-08-23 17:22:32 -0300

    add: notas Etapa 2-A1 e resolução excercício 05

---
+++ ETP-2/A1/app.js
@@ -0,0 +1,21 @@
+//! obs: loop em array "iteração"
+
+// For loop
+console.log('- For Loop');
+const nNames = ['Linus', 'Ada', 'Bill'];
+for (let i = 0; i < nNames.length; i++)
+{
+    console.log(`Elemento ${i}: ${nNames[i]}`);
+}
+console.log('For loop concluído');
+console.log('\n');
+
+// While loop
+let i = 0;
+while (i < 5)
+{
+    console.log(`Loop ${i}`);
+    i++;
+}
+console.log('While loop concluído');
+console.log('\n');

a8241a36efa0a6c6c069057b6d3e19e92e859909
Humberto Guedes <franciscohguedes@gmail.com>
2022-08-24 11:48:47 -0300

    add: notas etapa 2-A2 e resolucão exercício 06

---
+++ ETP-2/A2/app.js
@@ -0,0 +1,11 @@
+// if e else
+const password = 'oi123';
+
+if (password.length >= 8)
+{
+    console.log('essa senha tem 8 ou mais caracteres');
+}
+else
+{
+    console.log('a senha deve conter 8 ou mais caracteres');
+}
